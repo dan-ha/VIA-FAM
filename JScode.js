b06419f883d83b8f918e365858f208ff0b39683d
martinIvovIv <240246@via.dk>
2019-10-02 14:11:13 +0300

    I made all of the classes , needs a bit of work

--- JScode.js
+++ JScode.js
@@ -120,3 +120,114 @@ class Facilitator {
         return Facilitator(this.FirstName, this.LastName, this.password)
     }
 }
+
+// Appointment for the VAM
+class Appointment{
+    constructor(name, Facility, Student, Facilitator, time, date, location){
+        this.name = name
+        this.Facility = Facility
+        this.Student = Student
+        this.Facilitator = Facilitator
+        this.time = time
+        this.date = date
+        this.location = location
+    }
+    // returns the name of the appointment
+    getName(){
+        return this.name
+    }
+    // returns the facility to which the appointment belongs
+    getFacility(){
+        return this.Facility
+    }
+    //returns the student who created the appointment
+    getStudent(){
+        return this.Student
+    }
+    // returns the Facilitator of the appointment
+    getFacilitator(){
+        return this.Facilitator
+    }
+    // sets a new facilitator for the appointment
+    setFacilitator(newFacilitator){
+        this.Facilitator = newFacilitator
+    }
+    // returns the time of the appointment
+    getTime(){
+        return this.time
+    }
+    // sets a new time for the appointment
+    setTime(newTime){
+        this.time = newTime
+    }
+    // returns the date of the appointment
+    getDate(){
+        return this.date
+    }
+    // sets a new date for the appointment
+    setDate(newDate){
+        this.date = newDate
+    }
+    // returns the location of the appointment
+    getLocation(){
+        return this.location
+    }
+    // sets a new location for the appointment
+    setLocation(newLocation){
+        this.location = newLocation
+    }
+    //returns a string with all of the information of the appointment
+    toString(){
+        return "Appointment information: \n name: " +this.name + "belongs to the Facility: " + this.Facility
+        + "\nwas made by: " + this.Student + "\nhas " + this.Facilitator + " as facilitator\n" + "and it's going to be on" +
+        this.date + " at " + this.time + " on " + this.location
+    }
+    // returns a copy of the appointment
+    copy(){
+        return Appointment(this.name, this.Facility, this.Student, this.Facilitator, this.time, this.date, this.location)
+    }
+}
+
+class Service{
+    constructor(name, Facility, Facilitators){
+        this.name = name
+        this.Facility = Facility
+        this.Facilitators = Facilitators
+    }
+    //returns the name of the service
+    getName(){
+        return this.name
+    }
+    // returns the facility that the service belongs to
+    getFacility(){
+        return this.Facility
+    }
+    // sets a new facility that the service will belong to
+    setFacility(newFacility){
+        this.Facility = newFacility
+    }
+    // returns the facilitator from the facilitators array
+    getFacilitator(){
+        return this.Facilitators[0]
+    }
+    // returns the facilitators array
+    getFacilitators(){
+        return this.Facilitators
+    }
+    // sets a new array of facilitators
+    setFacilitators(newFacilitators){
+        this.Facilitators = newFacilitators
+    }
+    // adds a new facilitator
+    addFacilitator(newFacilitator){
+        this.Facilitators.add(newFacilitator)
+    }
+    // returns a toString with all of the information of the service
+    toString(){
+        return "the service " + this.name + " belongs to the " + this.Facility + " and has as facilitators " + this.Facilitators
+    }
+    // returns a copy of the Service
+    copy(){
+        return Service(this.name, this.Facility, this.Facilitators)
+    }
+}
