939e3f64d1715c79cf5b2524ebfa47687b4652a8
danha <daniel.hamarik@gmail.com>
2019-10-08 20:59:49 +0200

    1.6 frontend register facility

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Hello world!"/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+              <router-link to="/">Facility Appointment Manager</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <router-link to="/register">Register Facility</router-link>
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapActions } from "vuex";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'App',
+  mounted() {
+    this.fetchFacilities()
+  },
+  methods: {
+    ...mapActions(["fetchFacilities"])
   }
-}
+};
 </script>
---
+++ frontend/src/views/FacilityRegister.vue
@@ -0,0 +1,144 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="6" class="blue lighten-4">
+        <h1 style="text-align: center">Register new VIA Facility</h1>
+        <v-form 
+          ref="form" 
+          class="d-flex flex-column" 
+          v-model="valid"
+          lazy-validation>
+          <v-text-field 
+            v-model="name"
+            :rules="nameRules" 
+            counter="100" 
+            label="Facility name" >
+          </v-text-field>
+          <v-text-field 
+            v-model="description"
+            :rules="descriptionRules"
+            counter="1000" 
+            label="Description">
+          </v-text-field>
+
+          <OpeningHours ref="openingHours"></OpeningHours>
+
+          <div class="d-flex">
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="Email address"
+              prepend-icon="email"
+            ></v-text-field>
+            <v-text-field
+              v-model="phone"
+              :rules="phoneRules"
+              label="Phone number"
+              prepend-icon="phone"
+            ></v-text-field>
+          </div>
+          <v-text-field 
+            v-model="location"
+            :rules="locationRules"
+            label="Location" 
+            prepend-icon="place">
+          </v-text-field>
+          <v-textarea
+            outlined
+            v-model="additionalInfo"
+            counter="1000"
+            label="Additional Information"
+          ></v-textarea>
+          <v-btn 
+            :disabled="!valid"
+            color="primary" 
+            @click="submit">
+            Submit
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import OpeningHours from "@/components/OpeningHours.vue";
+import axios from "axios";
+
+export default {
+  name: "FacilityRegister",
+  data: () => ({
+    loading: false,
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length >= 3) || 'Name must be 3 or more characters',
+      v => (v &&  v.length <= 100) || 'Name must be less than 100 characters'
+    ],
+    description: "",
+    descriptionRules: [
+      v => !!v || 'Description is required'
+    ],
+    email: "email@address.com",
+    emailRules: [
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+    phone: "45000000",
+    phoneRules: [
+      v => /^[0-9]*$/.test(v) || "Phone number must be valid"
+    ],
+    location: "D.301",
+    locationRules: [
+      v => !!v || 'Location is required'
+    ],
+    additionalInfo: "Additional information about the facility"
+  }),
+  methods: {
+    ...mapActions(["fetchFacilities"]),
+    submit() {
+      if (this.$refs.form.validate()) {
+        this.register()
+      }
+    },
+    register: async function() {
+      this.loading = true;
+      try{
+        const response = await axios.post(`${process.env.VUE_APP_BACKEND_URL}/facility`, this.getFacility())
+        this.loading = false
+        console.log(response)
+        this.fetchFacilities();
+        this.$router.push('/')
+      }
+      catch (error) {
+        this.loading = false
+        console.log(error)
+        if (error.response) {
+            console.log(error.response.data); // => the response payload
+          }
+      }
+    },
+    getFacility: function() {
+      return {
+        name: this.name,
+        description: this.description,
+        openingHours: [
+          {
+            dayOfWeek: 1,
+            timeOpen: this.$refs.openingHours.timeFrom,
+            duration: 180
+          }
+        ],
+        emailAddress: this.email,
+        phoneNo: this.phone,
+        location: this.location,
+        additionalInfo: this.additionalInfo
+      }
+    }
+  },
+  components: {
+    OpeningHours
+  }
+};
+</script>
