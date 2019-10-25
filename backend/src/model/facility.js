21e191df0d4f12cf479e3cee979a0318a06e9ae9
danha <daniel.hamarik@gmail.com>
2019-10-25 15:01:02 +0200

    updated database design

--- backend/src/model/facility.js
+++ backend/src/model/facility.js
@@ -1,7 +1,7 @@
 const sequelize = require('../db/sequelize.js')
 const Sequelize = require('sequelize')
 
-const Facility = sequelize.define('facility', {
+const Facility = sequelize.define('facilities', {
     name: {
         type: Sequelize.STRING,
         primaryKey: true
@@ -27,8 +27,8 @@ const Facility = sequelize.define('facility', {
         allowNull: true
     }
 }, {
-        timestamps: false,
-    })
+    timestamps: false,
+})
 
 const OpeningHours = sequelize.define('openingHours', {
     dayOfWeek: {
@@ -44,10 +44,61 @@ const OpeningHours = sequelize.define('openingHours', {
         allowNull: false
     }
 }, {
-        timestamps: false,
-    })
+    timestamps: false,
+})
+
+const Facilitator = sequelize.define('facilitators', {
+    employeeId: {
+        type: Sequelize.STRING,
+        primaryKey: true
+    }
+}, {
+    timestamps: false,
+})
+
+const Appointment = sequelize.define('appointments', {
+    date: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    location: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    subject: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    document: {
+        type: Sequelize.BLOB,
+        allowNull: true
+    }
+}, {
+    timestamps: false,
+})
+
+const UnavailableHours = sequelize.define('unavailableHours', {
+    date: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    duration: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+}, {
+    timestamps: false,
+})
 
 Facility.hasMany(OpeningHours)
+Facility.hasMany(Facilitator)
+Facilitator.hasMany(Appointment)
+Facilitator.hasMany(UnavailableHours)
 sequelize.sync()
+
 
 module.exports = { Facility, OpeningHours }
